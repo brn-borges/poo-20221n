59c3f515497485d6be3301fb36d2a6c8ffdb2d60
Bruno Borges <bruninho-borgesf@hotmail.com>
2022-03-26 22:36:27 -0300

    realização da lista 4 - ex0

--- Lista4/src/main/java/br/edu/fatecfranca/ex0/Diretor.java
+++ Lista4/src/main/java/br/edu/fatecfranca/ex0/Diretor.java
@@ -1,5 +1,39 @@
 package br.edu.fatecfranca.ex0;
 
-public class Diretor {
+public class Diretor extends Funcionario{
+    private float carro;
+    private String modelo;
+
+    public Diretor(float carro, String modelo, int nro, String nome, float salario) {
+        super(nro, nome, salario);
+        this.carro = carro;
+        this.modelo = modelo;
+    }
+
+    public Diretor() {
+    }
+
+    public float getCarro() {
+        return carro;
+    }
+
+    public void setCarro(float carro) {
+        this.carro = carro;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Diretor{" + "carro=" + carro + ", modelo=" + modelo + super.toString() +'}';
+    }
+    
+    
     
 }
--- Lista4/src/main/java/br/edu/fatecfranca/ex0/Gerente.java
+++ Lista4/src/main/java/br/edu/fatecfranca/ex0/Gerente.java
@@ -1,5 +1,27 @@
 package br.edu.fatecfranca.ex0;
 
-public class Gerente {
+public class Gerente extends Funcionario{
+    private float comissao;
+
+    public Gerente(float comissao, int nro, String nome, float salario) {
+        super(nro, nome, salario);
+        this.comissao = comissao;
+    }
+
+    public Gerente() {
+    }
+
+    public float getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(float comissao) {
+        this.comissao = comissao;
+    }
+
     
+    @Override
+    public String toString() {
+        return "Gerente{" + "comissao=" + comissao + super.toString() +'}';
+    }    
 }
